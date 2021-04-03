2a61a4c8599f2dc455ddbfac55095a87d4d0aa7f
D7K7 <j3susf1shy@gmail.com>
2021-04-02 19:44:35 -0500

    Added clock functionality.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import { useNewTimer } from 'react';
 import './App.css';
 
 function App() {
+
+  const timer = useNewTimer(new Date());
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h2>It is {timer.toLocaleTimeString()}.</h2>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/D7K7"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Now back to my Github.
         </a>
       </header>
     </div>
---
+++ src/useNewTimer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function useNewTimer(currentDate) {
+    const [date, setDate] = React.useState(currentDate);
+    
+    React.useEffect(() => {
+      var timerID = setInterval( () => tick(), 1000 );
+      return function cleanup() {
+          clearInterval(timerID);
+        };
+     });
+    
+    function tick() {
+      setDate(new Date());
+     }
+    
+    return date;
+  }
+export default useNewTimer;
