22c464bb6cd80e795b40b1089fb58f3e7c83cdaf
D7K7 <j3susf1shy@gmail.com>
2021-04-02 21:57:02 -0500

    Updated clock functionality

---
+++ src/Clock.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+
+function Clock(props) {
+
+  const [date, setDate] = useState(new Date());
+
+ useEffect(() => {
+  var timerID = setInterval( () => tick(), 1000 );
+
+  return function cleanup() {
+      clearInterval(timerID);
+    };
+ });
+
+   function tick() {
+    setDate(new Date());
+   }
+
+  return (
+    <div className="App">
+      <header className="App-header">
+      The time is {date.toLocaleTimeString()}
+        <a
+          className="App-link"
+          href="https://github.com/D7K7"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Now back to my Github.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Clock;
